17d318bc2d870a2361a5e4996c36c2c5b83df891
kaito-miyamoto-macho <t.ali.j.kk@gmail.com>
2025-10-18 11:35:27 +0900

    Update test environment URL in main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function getGASUrl() {
         // 本番環境
         ? "https://script.google.com/macros/s/AKfycbw_qZ108jgUiDIIzmaPW6vCB9oVI24qRYpyE36qNVsRdHCpwXzP9Dbz0DmdpGBwR9Mk/exec"
         // テスト環境
-        : "https://script.google.com/macros/s/AKfycbyb7Q8TjYAdHQFWk49PHP-m6cU0E0LozCKiLY5l0WFRIzUOIyZqG3o8_GvpFk-phbOX/exec";
+        : "https://script.google.com/macros/s/AKfycbxGolAeP3XRBynMDthJQK0llczbHXgyVsFpOM1CIY4C8sZy3YgMjEfMCnPi_U5QgOoY/exec";
 }
 
 // ✅ URLパラメータを取得する関数
